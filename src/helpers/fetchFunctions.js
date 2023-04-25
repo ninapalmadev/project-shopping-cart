cf0c9c7b2605ab43d70e9970ab423fd853dc811e
Nina Palma <ninapalmadev@gmail.com>
2023-04-25 18:30:48 -0300

    requisito 3

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -4,12 +4,8 @@ export const fetchProduct = () => {
 
 export const fetchProductsList = async (QUERY) => {
   if (!QUERY) throw new Error('Termo de busca não informado');
-  try {
-    const URL_API = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
-    const response = await fetch(URL_API);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    return error.message;
-  }
+  const URL_API = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  const response = await fetch(URL_API);
+  const data = await response.json();
+  return data.results;
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+// Requisito 3
+const loadProducts = async () => {
+  const sectionProducts = document.querySelector('.products');
+  const listProducts = await fetchProductsList('computador');
+  // console.log(listProducts);
+  listProducts.forEach((product) => {
+    const item = createProductElement(product);
+    sectionProducts.appendChild(item);
+  });
+};
+
+loadProducts();
