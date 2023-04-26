e36f0a0d221a4216e0a314ade4ad3a9348508fb8
Nina Palma <ninapalmadev@gmail.com>
2023-04-26 12:19:52 -0300

    requisito 4

--- src/main.js
+++ src/main.js
@@ -4,16 +4,31 @@ import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const sectionProducts = document.querySelector('.products');
+
+// Requisito 4
+const putLoading = () => {
+  const span = document.createElement('span');
+  span.innerText = 'carregando...';
+  span.classList.add('loading');
+  sectionProducts.appendChild(span);
+};
+
+const removeLoading = () => {
+  const span = document.querySelector('.loading');
+  span.remove();
+};
 
 // Requisito 3
 const loadProducts = async () => {
-  const sectionProducts = document.querySelector('.products');
+  putLoading();
   const listProducts = await fetchProductsList('computador');
   // console.log(listProducts);
   listProducts.forEach((product) => {
     const item = createProductElement(product);
     sectionProducts.appendChild(item);
   });
+  removeLoading();
 };
 
 loadProducts();
